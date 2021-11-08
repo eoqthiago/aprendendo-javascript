5316feaf1c65885a182922d81e78af1339435442
eoqthiago <contatopixthiago@gmail.com>
2021-11-08 07:13:06 -0300

    matematica

---
+++ aula10/index.js
@@ -0,0 +1,13 @@
+/*
+ Aritméticos + - / 
+ Potenciação = **
+ Resto da Divisão = %
+*/
+
+const num1 = 5;
+const num2 = 5;
+console.log(num1 % num2);
+
+
+let contador = 1;
+contador = contador + 2;
