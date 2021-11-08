f1714ef6aa855d4b555f230906445f2aa8fb1962
eoqthiago <contatopixthiago@gmail.com>
2021-11-08 08:46:14 -0300

    number

---
+++ aula11/js/script.js
@@ -0,0 +1,7 @@
+const numero1 = prompt('Digite o Primeiro Numero: ');
+const numero2 = prompt('Digite o Segundo Numero: ');
+let n1 = Number(numero1);
+let n2 = Number(numero2);
+
+const resultado = n1 + n2;
+console.log(resultado);
--- testes.js
+++
@@ -1,1 +0,0 @@
-console.log('Meu Nome é "NOME". Estou aprendendo JavaScript às', 10, 'da manhã');
