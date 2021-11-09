03e49f1608204eeaccfcc333de9c8be42321a68d
eoqthiago <contatopixthiago@gmail.com>
2021-11-09 10:44:02 -0300

    m

---
+++ aula17/index.js
@@ -0,0 +1,25 @@
+const alunos = [ 'aa', 'thiago', 'luiz'];
+
+
+
+
+
+console.log(typeof alunos);
+
+//alunos.push('th');
+//alunos.push('lu');
+
+//console.log(alunos.slice(0, -2));
+
+//delete alunos [1];
+//console.log(alunos);
+
+//alunos.pop();
+
+//console.log(alunos);
+
+//alunos[0] =' salve';
+//alunos[3] =' sa';
+//console.log(alunos);
+
+//console.log(alunos[1]);
