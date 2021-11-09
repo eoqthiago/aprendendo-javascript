b382cc0e455d0655266d0a428ddf827a5745f90b
eoqthiago <contatopixthiago@gmail.com>
2021-11-09 09:23:22 -0300

    att

--- aula16/index.js
+++ aula16/index.js
@@ -1,9 +1,11 @@
-const numero = prompt('digite Um Numero: ');
-const decimal = Math.round(numero).toFixed(2);
+const numero = Number(prompt('digite Um Numero: '));
+const decimal = numero.toFixed(2);
 
-document.body.innerHTML += `Raiz Quadrada ${Math.pow(numero, 1/2)} <br>`; 
-document.body.innerHTML += `${numero} é inteiro? ${Number.isInteger(numero)} <br>`;
-document.body.innerHTML += `é NaN ? ${Number.isNaN(numero)} <br>`;
-document.body.innerHTML += ` Arredondando para baixo: ${Math.floor(numero)} <br>`;
-document.body.innerHTML += ` Arredondando para baixo: ${Math.ceil(numero)} <br>`;
-document.body.innerHTML += ` Com Duas Casas Decimais ${decimal} <br>`;
+const numerotitulo = document.getElementById('titulo');
+numerotitulo.innerHTML = numero;
+texto.innerHTML += `Raiz Quadrada ${Math.pow(numero, 1/2)} <br>`; 
+texto.innerHTML += `${numero} é inteiro? ${Number.isInteger(numero)} <br>`;
+texto.innerHTML += `é NaN ? ${Number.isNaN(numero)} <br>`;
+texto.innerHTML += ` Arredondando para baixo: ${Math.floor(numero)} <br>`;
+texto.innerHTML += ` Arredondando para baixo: ${Math.ceil(numero)} <br>`;
+texto.innerHTML += ` Com Duas Casas Decimais ${decimal} <br>`;
