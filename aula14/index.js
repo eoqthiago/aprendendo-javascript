1a6614f3e54782203a8ab59339357b8603f3cb2a
eoqthiago <contatopixthiago@gmail.com>
2021-11-09 07:00:43 -0300

    aula14

---
+++ aula14/index.js
@@ -0,0 +1,16 @@
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2;
+num1 += num2;
+num1 += num2;
+console.log(num1);
+
+// console.log( num1.toString() + num2);
+// num1 = num1.tostring();
+//console.log(num1.toFixed(2));
+//console.log(Number.isInteger(num1));  VER SE O NUMERO É INTEIRO OU NÃO!
+// NaN = Not A Number!
+//console.log(Number.isNaN(temp)); // SE A CONTA FOR INVALIDA ELE VAI FALAR TRUE
+// let temp = num1 + '5'; NÃO FAÇA CONTA COM STRING
+//console.log(temp);
