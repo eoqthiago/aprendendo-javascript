8466e67cab1d34cda55cce90e903cd15a4644bf7
eoqthiago <contatopixthiago@gmail.com>
2021-11-10 08:01:04 -0300

    a

---
+++ aula18/index.js
@@ -0,0 +1,28 @@
+//const pessoa = {
+//    nome: 'luiz',
+//    sobrenome: 'afern',
+// //   idade: 32,
+//
+//    fala() {
+//        console.log(`My Corrent Age Is ${this.idade}`);
+//    },
+//
+//    incrementaIdade(){//
+//      this.idade++;
+//    }
+//};
+//pessoa.fala();
+//pessoa.incrementaIdade();
+//pessoa.fala();
+
+
+//function criarPerfil (nome, sobrenome, idade){
+//    return {
+//        nome: nome,
+//        sobrenome: sobrenome,
+//        idade: idade
+//    };
+//}
+//
+//const pessoa1 = criarPerfil('Thiago', 'Otavo', 25);
+//console.log(pessoa1);
