fa9355fed279ace1c701e28d522ff6c1729fbfc2
eoqthiago <contatopixthiago@gmail.com>
2021-11-11 07:40:56 -0300

    operadores de comparacao e logicos

--- aula20/index.js
+++ aula20/index.js
@@ -11,8 +11,22 @@
 ******** OPERADORES LOGICOS **********
 --------------------------------------
 
-&& -> AND -> E
-|| -> OR -> OU
+&& -> AND -> E = TODAS AS EXPRESSÕES PRECISAM SER VERDADEIRAS PARA RETORNAR TRUE
+const exp = true & true & true;
+
+|| -> OR -> OU 
+const exp = false || true;
+
 ! -> NOT -> NÃO
+const exp =
+
 
 */
+
+//const usuario = 'usu';
+//const senha = '1234';
+//
+//const login = usuario === 'usu' && senha ==='1234';
+//console.log(login);
+
+console.log(!true);
