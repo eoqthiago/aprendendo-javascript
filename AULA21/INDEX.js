d4dc79840bf633bb946cc354cc6f853e82f9bff6
eoqthiago <contatopixthiago@gmail.com>
2021-11-12 15:01:49 -0300

    if e else e outras coisas

--- AULA21/INDEX.js
+++ AULA21/INDEX.js
@@ -5,3 +5,13 @@
 || -> FALSO
 
 */
+
+const hora = 1;
+
+if (hora > 0 && hora <= 11) {
+    console.log('Bom Dia.');
+} else if (hora >= 12 && hora <= 17 ) {
+    console.log('boa tarde!');
+} else if (hora >= 18 && hora <= 23) {
+    console.log('Boa Noite');
+}
