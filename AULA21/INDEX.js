73e2499245f798caa1971b2929abc1d4a8909cc7
eoqthiago <contatopixthiago@gmail.com>
2021-11-13 14:45:51 -0300

    if e else

--- AULA21/INDEX.js
+++ AULA21/INDEX.js
@@ -6,14 +6,15 @@
 
 */
 
-const hora = 100;
+//const hora = 100;
 
-if (hora > 0 && hora <= 11) {
-    console.log('Bom Dia.');
-} else if (hora >= 12 && hora <= 17 ) {
-    console.log('boa tarde!');
-} else if (hora >= 18 && hora <= 23) {
-    console.log('Boa Noite');
-} else{
-    console.log('essa hora esta errada');
-}
+//if (hora > 0 && hora <= 11) {
+//    console.log('Bom Dia.');
+//} else if (hora >= 12 && hora <= 17 ) {
+//    console.log('boa tarde!');
+//} else if (hora >= 18 && hora <= 23) {
+//    console.log('Boa Noite');
+//} else{
+//    console.log('essa hora esta errada');
+//}
+
