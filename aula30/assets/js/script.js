bfb6f8403e8f7ef23e1b0a4d155f14653f136f81
eoqthiago <contatopixthiago@gmail.com>
2021-11-25 16:09:45 -0300

    morrendo

--- aula30/assets/js/script.js
+++ aula30/assets/js/script.js
@@ -69,15 +69,13 @@ h1.innerHTML = criaData(data); */
 const h1 = document.querySelector('.container h1');
 const data = new Date();
 
-const opcoes = {
-    dateStyle: 'full',
-    //timeStyle: 'short'
+
     
     
-};
 
 
-h1.innerHTML = data.toLocaleDateString('pt-BR', opcoes);
+
+h1.innerHTML = data.toLocaleDateString('pt-BR', {dataStyle: 'full',});
 
 
 
